6392324da2a02d4a2f001a1c00cb5264ad024f33
hugo salazar <hugog.salazarl@gmail.com>
2024-10-11 17:11:30 -0500

    added About section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import Header from './components/Header'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <main>
       <Hero />
+      <About />
 
       </main>
     </>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,16 @@
-
+// Components
+import { ButtonPrimary, ButtonOutline } from "./Button"
 const Hero = () => {
   return (
     <section
     id="home"
-    className=""
+    className="pt-28 lg:pt-36"
     >
 
-      <div className="">
+      <div className="container lg:grid lg:grid-cols-2 items-center lg:gap-10">
         <div>
-        <div className="">
-          <figure className="">
+        <div className="flex items-center gap-3">
+          <figure className="img-box w-9 h-9 rounded-lg">
             <img
             src="/public/images/avatar-1.jpg"
             width={40}
@@ -18,26 +19,33 @@ const Hero = () => {
             className="img-cover"
             />
           </figure>
-          <div className="">
-            <span className="">
-              <span className=""></span>
+          <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+            <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+              <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
             </span>
 
             Available for work
           </div>
         </div>
-        <h2 className="">
+        <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
         Building Scalable Modern Websites for the Future
         </h2>
-        <div className="">
-          ButtonPrimary
+        <div className="flex items-center gap-3">
+          <ButtonPrimary
+           label="Download CV"
+           icon="download"
+          />
 
-          ButtonOutline
+          <ButtonOutline
+            href="#about"
+            label="Scroll down"
+            icon="arrow_downward"
+          />
         </div>
         </div>
-        <div className="/public/images/hero-banner.png">
-          <figure className="">
-            <img src=""
+        <div className="hidden lg:block">
+          <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
+            <img src="/images/hero-banner.png"
             width={656}
             height={800}
             alt="Hugo Salazar"
